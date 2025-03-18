798d10be1caf5aa6d7a0c44b6aa3b0b4ebd2db1c
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-18 18:28:30 +0300

    ProductCard

--- Client/src/components/Product.tsx
+++ Client/src/components/Product.tsx
@@ -1,4 +1,13 @@
 import { IProduct } from "../model/IProduct";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { AddShoppingCart, Search } from "@mui/icons-material";
 
 interface Props {
   product: IProduct;
@@ -6,13 +15,44 @@ interface Props {
 
 export default function Product({ product }: Props) {
   return (
-    <>
-      {product.isActive && (
-        <>
-          <h3>{product.name}</h3>
-          <p>{product.price}</p>
-        </>
-      )}
-    </>
+    <Card>
+      <CardMedia
+        sx={{ height: 160, backgroundSize: "contain" }}
+        component="img"
+        image={`http://localhost:5226/images/${product.imageUrl}`}
+      />
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="h6"
+          component="h2"
+          color="text.secondary"
+        >
+          {product.name}
+        </Typography>
+        <Typography variant="body2" color="secondary">
+          {(product.price / 100).toFixed(2)} TL
+        </Typography>
+      </CardContent>
+
+      <CardActions>
+        <Button
+          variant="outlined"
+          size="small"
+          startIcon={<AddShoppingCart />}
+          color="success"
+        >
+          Add to cart
+        </Button>
+        <Button
+          variant="outlined"
+          size="small"
+          startIcon={<Search />}
+          color="primary"
+        >
+          View
+        </Button>
+      </CardActions>
+    </Card>
   );
 }

f179cd3db403446bb50e611d6d81567bb373d611
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-23 21:22:46 +0300

    AxiosResponse

--- Client/src/pages/catalog/CatalogPage.tsx
+++ Client/src/pages/catalog/CatalogPage.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
 import { IProduct } from "../../model/IProduct";
 import ProductList from "./ProductList";
+import requests from "../../api/Request";
+import { CircularProgress } from "@mui/material";
 
 export default function CatalogPage() {
   const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:5226/api/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
+    requests.Catalog.list()
+      .then((data) => setProducts(data))
+      .finally(() => setLoading(false));
   }, []);
+
+  if (loading) return <CircularProgress />;
+
   return <ProductList products={products} />;
 }

a1740382b0cf8d0a7a053eb4920cdb93e9f1a741
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-26 02:12:03 +0300

    Genel

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35dba3e0e2eca733f539cc789e345ba76a9117f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f179cd3db403446bb50e611d6d81567bb373d611")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

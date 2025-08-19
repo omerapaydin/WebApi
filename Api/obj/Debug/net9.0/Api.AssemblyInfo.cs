f2816c9e1b04a889db876467e5c7ff391e7d7e5d
Ömer Apaydın <omerapaydin90@gmail.com>
2025-08-20 00:48:14 +0300

    Entity

--- Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e195a5c3ec88d5420f17e98fb3afa3b94af243b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+763e772c86ece321ba90a2bf376376999cbe55c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

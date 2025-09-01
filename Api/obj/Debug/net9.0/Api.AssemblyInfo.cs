916f0cabb4be7c7ad943fb6d79af6f8948a9f3ab
Ömer Apaydın <omerapaydin90@gmail.com>
2025-09-02 00:00:56 +0300

    React

--- Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba35895cd94de04a922e6218b8dd0522d769a1c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ca637254fc858ba1aacecc88dea73153c71041")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

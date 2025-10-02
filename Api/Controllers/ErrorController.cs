20ac5046ce491fe89b429b45c51a2b5394d09396
Ömer Apaydın <omerapaydin90@gmail.com>
2025-10-02 22:40:32 +0300

    ErrorController

---
+++ Api/Controllers/ErrorController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ErrorController : ControllerBase
+    {
+         [HttpGet("not-found")]
+        public IActionResult NotFoundError()
+        {
+            return NotFound(); //404
+        }
+
+        [HttpGet("bad-request")]
+        public IActionResult BadRequestError()
+        {
+            return BadRequest(); //400
+        }
+
+        [HttpGet("unauthorized")]
+        public IActionResult UnAuthorizedError()
+        {
+            return Unauthorized(); //401
+        }
+
+        [HttpGet("validation-error")]
+        public IActionResult ValidationError()
+        {
+            ModelState.AddModelError("validation error 1","validation error details");
+            ModelState.AddModelError("validation error 2","validation error details");
+            return ValidationProblem();
+        }
+
+        [HttpGet("server-error")]
+        public IActionResult ServerError()
+        {
+            throw new Exception("server error");
+        }
+    }
+}
